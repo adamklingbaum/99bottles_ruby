01deb49c5ca32cba43d18d91c74d1930b6637390
Adam Klingbaum <adam.klingbaum@shopify.com>
2022-06-04 20:32:23 -0400

    Solution 1
    Incomprehensibly Concise

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'https://rubygems.org'
 gem 'minitest', '~> 5.4'
 gem 'pry'
+
+gem "ruby-lsp", require: false
--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,51 +1,19 @@
 class Bottles
-  def verse(bottles)
-    "#{beer_on_the_wall(bottles)}\n"+
-      "#{take_down(bottles)}" +
-      "#{bottles_remaining(bottles - 1)}\n"
-  end
-
-  def verses(from, to)
-    verses = from.downto(to).map { |x| verse(x) }
-    verses.join("\n")
-  end
-
   def song
-    verses(99,0)
+    verses(99, 0)
   end
 
-  def bottles_remaining(bottles)
-    if bottles == 1
-      "1 bottle of beer on the wall."
-    elsif bottles == 0
-      "no more bottles of beer on the wall."
-    elsif bottles < 0
-      "99 bottles of beer on the wall."
-    else
-      "#{bottles} bottles of beer on the wall."
-    end
+  def verses(hi, lo)
+    hi.downto(lo).map { |n| verse(n) }.join("\n")
   end
 
-  def beer_on_the_wall(bottles)
-    if bottles == 1
-      "1 bottle of beer on the wall, " +
-      "1 bottle of beer."
-    elsif bottles == 0
-      "No more bottles of beer on the wall, " +
-      "no more bottles of beer."
-    else
-      "#{bottles} bottles of beer on the wall, " +
-      "#{bottles} bottles of beer."
-    end
-  end
-
-  def take_down(bottles)
-    if bottles == 1
-      "Take it down and pass it around, "
-    elsif bottles == 0
-      "Go to the store and buy some more, "
-    else
-      "Take one down and pass it around, "
-    end
+  def verse(n)
+    "#{n == 0 ? 'No more' : n} bottle#{'s' if n != 1}" +
+    " of beer on the wall, " +
+    "#{n == 0 ? 'no more' : n} bottle#{'s' if n != 1} of beer.\n" +
+    "#{n > 0 ? "Take #{n > 1 ? 'one' : 'it'} down and pass it around"
+      : "Go to the store and buy some more"}, " +
+    "#{n-1 < 0 ? 99 : n-1 == 0 ? 'no more' : n-1} bottle#{'s' if n-1 != 1}" +
+    " of beer on the wall.\n"
   end
 end
